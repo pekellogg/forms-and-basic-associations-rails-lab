6eb05faf98f1020586c5202789a31f78f60692e3
Peyton Kellogg <pekellogg@hotmail.com>
2022-05-18 15:23:10 -0700

    passing specs

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,24 @@
 class Song < ActiveRecord::Base
-  # add associations here
+  belongs_to :artist
+  belongs_to :genre
+  has_many :notes
+
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by(name: name)
+  end
+
+  def artist_name
+    self.artist ? self.artist.name : nil
+  end
+
+  def note_contents=(contents_arr)
+    contents_arr.each do |content|
+      new_note = Note.create(content: content) unless content == ""
+      self.notes << new_note unless new_note.nil?
+    end
+  end
+
+  def note_contents
+    self.notes.map(&:content)
+  end
 end
